f4221e620f1113a3b12ee2b2224457b11ed99a7c
FUJI Goro <goro@fastly.com>
2022-01-22 16:15:32 +0900

    add a feature to clear the local storage

--- src/options.tsx
+++ src/options.tsx
@@ -3,28 +3,42 @@ import ReactDOM from "react-dom";
 
 import manifest from "../public/manifest.json";
 
-type OptionsMainProps = {
-};
+type OptionsMainProps = {};
 
-const OptionsMain: React.FC<OptionsMainProps> = ({
-}) => {
-  return <main>
-    <h1>${manifest.name}</h1>
-    <section>
-      <p>
-        Welcome to ${manifest.name}!
-      </p>
-    </section>
-  </main>;
+const OptionsMain: React.FC<OptionsMainProps> = ({}) => {
+  const [bytesInUse, setBytesInUse] = useState(0);
+
+  useEffect(() => {
+    chrome.storage.local.getBytesInUse(null, (bytes) => {
+      setBytesInUse(bytes);
+    });
+  }, []);
+
+  return (
+    <main>
+      <h1>{manifest.name} v{manifest.version}</h1>
+      <hr />
+      <section>
+        <h2>Clear the Local Storage</h2>
+        <p>It consumes {bytesInUse} bytes.</p>
+        <p>
+          <button onClick={() => {
+            chrome.storage.local.clear();
+            chrome.storage.local.getBytesInUse(null, (bytes) => {
+              setBytesInUse(bytes);
+            });
+          }}>clear</button>
+        </p>
+      </section>
+      <hr />
+    </main>
+  );
 };
 
 const OptionsApp: React.FC = () => {
-  return <OptionsMain/>;
+  return <OptionsMain />;
 };
 
 window.addEventListener("DOMContentLoaded", async () => {
-  ReactDOM.render(
-    <OptionsApp />,
-    document.getElementById("app"),
-  );
+  ReactDOM.render(<OptionsApp />, document.getElementById("app"));
 });
